18507fc2648ab294276c150ac22cc12eb577f710
nguyenminhtu <tuunguyen2795@gmail.com>
2020-09-12 11:43:33 +0700

    fix 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import List from "./List";
+import New from "./New";
+import Detail from "./Detail";
 
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/detail">
+          <Detail />
+        </Route>
+        <Route path="/new">
+          <New />
+        </Route>
+        <Route path="/">
+          <List />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
