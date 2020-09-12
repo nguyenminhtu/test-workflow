9194491ad745a2f4762bdda35a409c6e5c719349
nguyenminhtu <tuunguyen2795@gmail.com>
2020-09-12 11:38:43 +0700

    init

---
+++ src/Detail.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+
+export default function Detail() {
+  return (
+    <Container>
+      <Card></Card>
+    </Container>
+  );
+}
